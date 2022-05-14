659021fd27ccffba24a28577c715a56fb3ae4b30
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2022-05-14 01:32:43 -0400

    navbar skeleton placed in after various rremodels

--- src/Parts/Navbar.jsx
+++ src/Parts/Navbar.jsx
@@ -1,51 +1,47 @@
 import React from 'react'
 
-function Navbar() {
+const Navbar = () => {
+
+
   return (
-    <div class="top-0 sticky bg-slate-800 text-white py-4">
+   
+  <div class="top-0 sticky top-0 z-30 w-full  px-1 py-1 bg-pink-900 shadow-xl shadow-orange-600/20 sm:px-1">
 
-<header class="shadow-sm">
-  <div class="max-w-screen-xl p-4 mx-auto">
-    <div class="flex items-center justify-between space-x-4 lg:space-x-10">
-      <div class="flex lg:w-0 lg:flex-1">
-        <span class="w-20 h-10 bg-gray-200 rounded-lg"></span>
-      </div>
-
-      <nav class="hidden space-x-8 text-sm font-medium md:flex">
-        <a class="text-gray-500" href="">About</a>
-        <a class="text-gray-500" href="">Blog</a>
-        <a class="text-gray-500" href="">Projects</a>
-        <a class="text-gray-500" href="">Contact</a>
-      </nav>
-
-      <div class="items-center justify-end flex-1 hidden space-x-4 sm:flex">
-        <h1>pow</h1>
-      </div>
-
-      <div class="lg:hidden">
-        <button class="p-2 text-gray-600 bg-gray-100 rounded-lg" type="button">
-          <span class="sr-only">Open menu</span>
-          <svg
-            aria-hidden="true"
-            class="w-5 h-5"
-            fill="none"
-            stroke="currentColor"
-            viewbox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M4 6h16M4 12h16M4 18h16"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-            />
-          </svg>
-        </button>
-      </div>
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <div class="dropdown">
+      
+      <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Contact</a></li>
+        <li tabindex="0">
+          <a class="justify-between">
+            Parent
+            
+          </a>
+          
+        </li>
+       
+      </ul>
     </div>
+    <div>Spinning A</div>
   </div>
-</header>
-
+  <div class="navbar-center hidden lg:flex">
+    <ul class="menu menu-horizontal p-0">
+      <li><a>Contact</a></li>
+      <li tabindex="0">
+        <a>
+         Work
+          
+        </a>
+        
+      </li>
+      <li><a>About</a></li>
+    </ul>
+  </div>
+  <div class="navbar-end">
+    <div>Spinning M</div>
+  </div>
+</div>
 
 
 
