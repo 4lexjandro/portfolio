51c702e35cf0594bd60ccd7a90e9b980b826ca44
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2022-05-13 23:57:29 -0400

    routes added, not sure if i want to put a navbar or not

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './Routes/Home'
+import Contact from './Routes/Contact'
+import NotFound from './Routes/NotFound'
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>Start over.......#frustration</p>
-      
-    </div>
+    
+   <Router>
+     <Routes>
+     <Route path='/' element={<Home />} />
+     <Route path='/home' element={<Home />} />
+     <Route path='/contact' element={<Contact />} />
+     <Route path='/notfound' element={<NotFound />} />
+     <Route path='/*' element={<NotFound />} />
+     </Routes>
+   </Router>
   );
 }
 
