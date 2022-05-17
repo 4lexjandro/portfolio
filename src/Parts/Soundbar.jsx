3490619e7245e9e6ee81b0970407f3acb6873bd7
Alexjandro Mendoza <alexjandromendoza@macbook-pro.mynetworksettings.com>
2022-05-17 15:13:02 -0400

    fixed soundbar position

--- src/Parts/Soundbar.jsx
+++ src/Parts/Soundbar.jsx
@@ -7,10 +7,7 @@ const Box = styled.div`
   display: flex;
   cursor: pointer;
 
-  position: fixed;
-  left: 8rem;
-  top: 3rem;
-  z-index: 10;
+ 
 
   & > *:nth-child(1) {
     animation-delay: 0.2s;
