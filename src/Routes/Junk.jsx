03624c34c6a0e54dc1c1cd108655e55126c0ce94
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2022-06-06 23:51:29 -0400

    changing work page...again

--- src/Routes/Junk.jsx
+++ src/Routes/Junk.jsx
@@ -1,9 +1,135 @@
-import React from 'react'
+import rocket from '../Visuals/rocket2.png'
+import P1 from '../Visuals/P1.png'
+import P2 from '../Visuals/P2.png'
+import P3 from '../Visuals/P3.png'
+import P4 from '../Visuals/P4.png'
+import P5 from '../Visuals/P5.png'
+import P6 from '../Visuals/P6.png'
 
 function Junk() {
   return (
-    <div>Junk
-      File
+    <div>
+<br/><br/><br/><br/>
+
+<div class="container">
+
+
+     <div class="card">
+       <div class="face face1">
+         <div class="content">           
+           <img src ={P1} width="300px" height="300px" />
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="#" type="button">Read More</a>
+         </div>
+       </div>
+    </div>
+    
+    <div class="card">
+       <div class="face face1">
+         <div class="content">             <img src ={P2} width="300px" height="300px" />
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="#" type="button">Read More</a>
+         </div>
+       </div>
+    </div>
+    
+    
+    <div class="card">
+       <div class="face face1">
+         <div class="content">
+           <img src ={P3} width="300px" height="300px" />
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="#" type="button">Read More</a>
+         </div>
+       </div>
+    </div>
+
+
+
+    <div class="card">
+       <div class="face face1">
+         <div class="content">           
+           <img src ={P4} width="300px" height="300px" />
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="#" type="button">Read More</a>
+         </div>
+       </div>
+    </div>
+
+
+
+
+
+
+    <div class="card">
+       <div class="face face1">
+         <div class="content">           
+           <img src ={P5} width="300px" height="300px" />
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="#" type="button">Read More</a>
+         </div>
+       </div>
+    </div>
+
+
+
+
+
+
+    <div class="card">
+       <div class="face face1">
+         <div class="content">           
+           <img src ={P6} width="300px" height="300px" />
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="#" type="button">Read More</a>
+         </div>
+       </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+  </div>
+
+
     </div>
   )
 }
