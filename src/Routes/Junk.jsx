9ab240cd917f96d310aba87af6ec142d5df93f50
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2022-06-07 15:22:49 -0400

    added routes to images, now to add github repos

--- src/Routes/Junk.jsx
+++ src/Routes/Junk.jsx
@@ -5,6 +5,9 @@ import P3 from '../Visuals/P3.png'
 import P4 from '../Visuals/P4.png'
 import P5 from '../Visuals/P5.png'
 import P6 from '../Visuals/P6.png'
+import { FaGithub } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Junk() {
   return (
@@ -16,8 +19,36 @@ function Junk() {
 
      <div class="card">
        <div class="face face1">
-         <div class="content">           
-           <img src ={P1} width="300px" height="300px" />
+         <div class="content">   
+         <a href="https://re-sable.vercel.app/">
+         <img src ={P1} width="300px" height="300px" /> </a>        
+         
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           
+           
+             
+           <a href="https://github.com/4lexmendez/LovingYears"
+        className="git">
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+         </a>
+        
+             
+           
+           
+           
+         </div>
+       </div>
+    </div>
+    
+    <div class="card">
+       <div class="face face1">
+         <div class="content">     
+         <a href="https://agency-beige.vercel.app/">
+         <img src ={P2} width="300px" height="300px" /> </a>        
          </div>
        </div>
        <div class="face face2">
@@ -28,24 +59,12 @@ function Junk() {
        </div>
     </div>
     
-    <div class="card">
-       <div class="face face1">
-         <div class="content">             <img src ={P2} width="300px" height="300px" />
-         </div>
-       </div>
-       <div class="face face2">
-         <div class="content">
-           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
-           <a href="#" type="button">Read More</a>
-         </div>
-       </div>
-    </div>
-    
     
     <div class="card">
        <div class="face face1">
          <div class="content">
-           <img src ={P3} width="300px" height="300px" />
+         <a href="https://loving-years.vercel.app/">
+         <img src ={P3} width="300px" height="300px" /> </a>
          </div>
        </div>
        <div class="face face2">
@@ -61,26 +80,8 @@ function Junk() {
     <div class="card">
        <div class="face face1">
          <div class="content">           
-           <img src ={P4} width="300px" height="300px" />
-         </div>
-       </div>
-       <div class="face face2">
-         <div class="content">
-           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
-           <a href="#" type="button">Read More</a>
-         </div>
-       </div>
-    </div>
-
-
-
-
-
-
-    <div class="card">
-       <div class="face face1">
-         <div class="content">           
-           <img src ={P5} width="300px" height="300px" />
+         <a href="https://github-finder-pi-pink.vercel.app/">
+         <img src ={P4} width="300px" height="300px" /> </a>
          </div>
        </div>
        <div class="face face2">
@@ -99,7 +100,28 @@ function Junk() {
     <div class="card">
        <div class="face face1">
          <div class="content">           
-           <img src ={P6} width="300px" height="300px" />
+         <a href="https://mendozasupportcenter.herokuapp.com/">
+         <img src ={P5} width="300px" height="300px" /> </a>
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
+           <a href="#" type="button">Read More</a>
+         </div>
+       </div>
+    </div>
+
+
+
+
+
+
+    <div class="card">
+       <div class="face face1">
+         <div class="content">           
+         <a href="">
+         <img src ={P6} width="300px" height="300px" /> </a>
          </div>
        </div>
        <div class="face face2">
