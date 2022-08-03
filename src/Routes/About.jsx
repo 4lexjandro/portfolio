6153b59485b8be9ea333f956e6eed2c111960729
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2022-08-03 09:15:53 -0400

    minor change

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -11,7 +11,7 @@ import Spinner from '../Parts/Spinner.jsx'
 export const About = () => {
 
   const [loading] = useState(false)
-    
+
 
   if (loading) {
     return <Spinner />
@@ -25,10 +25,10 @@ export const About = () => {
 <div className="bg bg3"></div>
 <br/><br/><br/><br/><br/><br/><br/><br/><br/>
 
-         
-          <motion.div animate={{ x: 100, y: 0, rotate:360 }}   
+
+          <motion.div animate={{ x: 100, y: 0, rotate:360 }}
 transition={{ duration: 2, repeat: Infinity }} >Pushing</motion.div>
-          
+
           <h5>my creative boundaries</h5>
                <motion.div
                 drag
@@ -42,8 +42,8 @@ transition={{ duration: 2, repeat: Infinity }} >Pushing</motion.div>
                 Otherwise, life is a drag
                 </motion.div>
           <br/>
-          
-          <h3 className="h3"> 
+
+          <h3 className="h3">
           <motion.div animate={{ scale: [2, 2, 1] }} >
           <motion.div animate={{ x: 170, y: 50, rotate:90 }} >Web Development</motion.div>
           </motion.div>
@@ -54,19 +54,19 @@ transition={{ duration: 2, repeat: Infinity }} >Pushing</motion.div>
            <p>tools, not just to obtain them.</p>
            <br/>
 
-            <h3 className="h3"> 
-            <motion.div animate={{ scale: [2, 2, 1] }} > 
+            <h3 className="h3">
+            <motion.div animate={{ scale: [2, 2, 1] }} >
           <motion.div animate={{ x: -150, y:100, rotate:90 }} >Animations & Art</motion.div>
           </motion.div>
           </h3>
-            
+
               <p>I also am fascinated by 3D art</p>
               <p>Hopefully I can combine my</p>
-              <p>upshots with my web exp</p>
+              <p>upshots with my dev exp</p>
               <p>Short Films (& website) coming soon</p>
-            
-            <h3 className="h3"> 
-            <motion.div animate={{ scale: [2, 2, 1] }} > 
+
+            <h3 className="h3">
+            <motion.div animate={{ scale: [2, 2, 1] }} >
           <motion.div animate={{ x: 150, y: 100, rotate:90 }} >Wealth</motion.div>
           </motion.div>
           </h3>
@@ -75,8 +75,8 @@ transition={{ duration: 2, repeat: Infinity }} >Pushing</motion.div>
             <p>consistently investing, discipline, &</p>
             <p>being open minded to new</p>
             <p>oppurtunities.</p>
-           </section> 
-          
+           </section>
+
            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
     <h1 className="boxes">SKILLSET</h1>
@@ -86,7 +86,7 @@ transition={{ duration: 2, repeat: Infinity }} >Pushing</motion.div>
   <div className="box"><SiJavascript className="animate-bounce h-7 w-7"/></div>
   <div className="box"><FaPython className="animate-bounce h-7 w-7"/></div>
   <div className="box"><FaHtml5 className="animate-bounce h-7 w-7"/></div>
-  
+
   </div>
   <br/>
   <div className="boxes">
@@ -103,16 +103,16 @@ transition={{ duration: 2, repeat: Infinity }} >Pushing</motion.div>
   <div className="box"><DiNodejs className="animate-bounce h-7 w-7"/></div>
   <div className="box"><SiMongodb className="animate-bounce h-7 w-7"/></div>
   <div className="box"><SiFirebase className="animate-bounce h-7 w-7"/></div>
-  
+
   </div>
 <br/><br/><br/>
   <div>
     <Footer />
   </div>
-  
-   
+
+
       </main>
-      
+
   )
 }
 
