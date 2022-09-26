092426bb0baabb25ac2a71577e2c112e0b772340
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2022-09-25 23:01:30 -0400

    removed youtube, quite useless to have since i dont even use it

--- src/Parts/Social.jsx
+++ src/Parts/Social.jsx
@@ -23,10 +23,7 @@ export default function SocialFollow() {
         <FontAwesomeIcon icon={faGithub} size="2x" />
       </a>
 
-      <a href="https://www.youtube.com/channel/UCoEzp7t1xdHp7ItsaD30N8w"
-        className="bar">
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
-      </a>
+     
 
       <a href="https://twitter.com/4lexmendez/"
         className="bar">
