116f30b8c31463ebdf1af9bb054e41f4e7edc38e
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2023-11-02 18:42:10 -0400

    edited social

--- src/Parts/Social.jsx
+++ src/Parts/Social.jsx
@@ -21,21 +21,21 @@ export default function SocialFollow() {
     <div className="grid grid-flow-col gap-3 ">
        
       <a href="https://github.com/4lexjandro"
-        className="barOne">
+        className="barThree">
         <FontAwesomeIcon icon={faGithub} size="2x" />
       </a>
 
      
 
-      <a href="https://twitter.com/4lexjandro_m"
+      {/* <a href="https://twitter.com/4lexjandro_m"
         className="barTwo">
         <FontAwesomeIcon icon={faTwitter} size="2x" />
       </a>
 
       <a href="https://www.instagram.com/4lexjandro_m/"
-        className="barThree">
+        className="barOne">
         <FontAwesomeIcon icon={faInstagram} size="2x" />
-      </a>
+      </a> */}
 
       <a href="https://www.linkedin.com/in/alexjandro-mendoza-17b28a19b/" className="barFour">
         <FontAwesomeIcon icon={faLinkedin} size="2x" />
