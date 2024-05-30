673709dcc07111fdb1ca70c45f036c8a3eaaff89
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2024-05-29 22:30:55 -0400

    added aerial photo

--- src/Routes/Services/Air.jsx
+++ src/Routes/Services/Air.jsx
@@ -1,9 +1,21 @@
-// Air.jsx
 
-import Water from '../../Visuals/Water.JPG'; // Importing an example image
-import AMM from '../../Visuals/AMM.png'; // Importing an example image
-import Video from '../../Visuals/Video.MP4'; // Importing an example image
+import Video from '../../Visuals/Video.MP4';
 import Footer from '../../Parts/Footer'
+import Pic1 from '../../Visuals/Airpics/Pic1.png';
+import Pic2 from '../../Visuals/Airpics/Pic2.jpg';
+import Pic3 from '../../Visuals/Airpics/Pic3.png';
+import Pic4 from '../../Visuals/Airpics/Pic4.JPG';
+import Pic5 from '../../Visuals/Airpics/Pic5.JPG';
+import Pic6 from '../../Visuals/Airpics/Pic6.jpg';
+import Pic7 from '../../Visuals/Airpics/Pic7.JPG';
+import Pic8 from '../../Visuals/Airpics/Pic8.JPG';
+import Pic9 from '../../Visuals/Airpics/Pic9.JPG';
+import Pic10 from '../../Visuals/Airpics/Pic10.JPG';
+import Pic12 from '../../Visuals/Airpics/Pic12.JPG';
+import Pic13 from '../../Visuals/Airpics/Pic13.JPG';
+import Pic14 from '../../Visuals/Airpics/Pic14.JPG';
+
+
 
 // Define your array of images
 
@@ -14,119 +26,53 @@ const Air = () => {
     <main className='contextt'>
     <br/><br/><br/><br/><br/>
     <div className="grid-wrapper">
-	<div>
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div>
-		<img src={AMM} alt='' className="masonry" />
-	</div>
-
-
-    <video autoPlay loop muted playsInline={true} 
-      controls={false}
-      disablePictureInPicture={true} 
-      id='video' className="vid"
-
-
-      
-      
-      
-      
-      
-      
-      >
-        <source
-          src={Video}
-          type="video/mp4"
-        />
-        </video>
-
-
-
-
+	
 
 
 	<div className="tall">
-		<img src={Water} alt='' className="masonry" />
+		<img src={Pic1} alt='' className="masonry" />
 	</div>
 	<div className="wide">
-    <img src={AMM} alt='' className="masonry" />
+    <img src={Pic2} alt='' className="masonry" />
 	</div>
 	<div>
-		<img src={Water} alt='' className="masonry" />
+		<img src={Pic3} alt='' className="masonry" />
+	</div>
+	
+	<div className="big">
+		<img src={Pic4} alt='' className="masonry" />
+	</div>
+	
+	<div className="wide">
+		<img src={Pic5} alt='' className="masonry" />
+	</div>
+	<div className="big">
+    <img src={Pic7} alt='' className="masonry" />
 	</div>
 	<div className="tall">
-    <img src={AMM} alt='' className="masonry" />
-	</div>
-	<div className="big">
-		<img src={Water} alt='' className="masonry" />
+		<img src={Pic6} alt='' className="masonry" />
 	</div>
 	<div>
-    <img src={AMM} alt='' className="masonry" />
+		<img src={Pic8} alt='' className="masonry" />
 	</div>
+	<div>
+		<img src={Pic9} alt='' className="masonry" />
+	</div>
+	<div>
+		<img src={Pic10} alt='' className="masonry" />
+	</div>
+	
 	<div className="wide">
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div className="big">
-    <img src={AMM} alt='' className="masonry" />
-	</div>
-	<div className="tall">
-		<img src={Water} alt='' className="masonry" />
+		<img src={Pic12} alt='' className="masonry" />
 	</div>
 	<div>
-		<img src={Water} alt='' className="masonry" />
+		<img src={Pic13} alt='' className="masonry" />
 	</div>
 	<div>
-		<img src={Water} alt='' className="masonry" />
+		<img src={Pic14} alt='' className="masonry" />
 	</div>
-	<div>
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div>
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div className="wide">
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div>
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div>
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div className="wide">
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div>
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div className="wide">
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div className="big">
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div>
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div>
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div className="big">
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div className="tall">
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div>
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div>
-		<img src={Water} alt='' className="masonry" />
-	</div>
-	<div className="wide">
-		<img src={Water} alt='' className="masonry" />
-	</div>
+	
+	
     
 </div>
 <Footer/>
