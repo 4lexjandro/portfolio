c064a71d4970d1c9ce282c32e9a81887873024a6
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2024-07-10 12:21:55 -0400

    took out portfolio in footer

--- src/Parts/Footer.jsx
+++ src/Parts/Footer.jsx
@@ -34,7 +34,6 @@ const Footer = ( ) => {
   <nav className="grid grid-flow-col gap-3">
     <a href='/'className="link link-hover">Home</a>
     <a href='/contact'className="link link-hover">Contact</a>
-    <a href='/work'className="link link-hover">Portfolio</a>
     <a href='/services'className="link link-hover">Services</a>
     {/* <a href='/prints'className="link link-hover">Prints</a> */}
   </nav> 
